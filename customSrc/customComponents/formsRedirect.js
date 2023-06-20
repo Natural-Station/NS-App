9b851a643ac8eb410ad55e38480601dc520124aa
Franco Aguilar <65577460+francoag2310@users.noreply.github.com>
2023-06-19 20:33:09 -0600

    save

---
+++ customSrc/customComponents/formsRedirect.js
@@ -0,0 +1,29 @@
+import {Button, Linking, View} from "react-native";
+import React from "react";
+
+const FormsRedirect = (props) => {
+    
+  const openForm1 = () => {
+    const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSc682QZyetUQ042hBuAx_lIRhhMQEbW6Xz9GZG1Bb-mTIaoUQ/viewform";
+    Linking.openURL(formUrl);
+  }
+
+  const openForm2 = () => {
+    const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfill6uORK8DfFWomabyUkIUhGJpnSF6pxXd-1LiMfIeZ8oGQ/viewform";
+    Linking.openURL(formUrl);
+  }
+
+  return <View style={{padding: 20, backgroundColor: "black", borderRadius: 5}}>
+    <Button
+        title="Ingresar Historia Clínica Nutricional"
+        onPress={openForm1}
+    />
+
+    <Button
+        title="Ingresar Medidas de Composición Corporal"
+        onPress={openForm2}
+    />
+  </View>
+}
+
+export default FormsRedirect;
